78c1207bd8e75c987551a91138d74acf5fdda4b7
aviaviavi <avipress@gmail.com>
2023-04-08 10:11:56 -0700

    Online indicator

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Create a map to store the mapping of studio ID to its connected users
 const studios = new Map();
+const userToStudio = new Map();
 
 // Generate a random ID for each studio
 function generateStudioId() {
@@ -37,6 +38,37 @@ io.on('connection', (socket) => {
     socket.join(studioId);
     socket.studioId = studioId;
     console.log(`User ${socket.id} joined studio: ${studioId}`);
+    // Add the socketId to the studio so that we can track the connected users
+    if (studios.has(studioId)) {
+      const studio = studios.get(studioId);
+      studio.push(socket.id);
+    } else {
+      studios.set(studioId, [socket.id]);
+    }
+    // Send the occupancy of the studio to all connected users
+    const studio = studios.get(studioId);
+    io.to(studioId).emit('studio-occupancy', { occupancy: studio.length });
+
+    // Add the user to the user to studio map
+    userToStudio.set(socket.id, studioId);
+  });
+
+  // Listen for users leaving the studio
+  socket.on('leave-studio', (data) => {
+    const studioId = data.studioId;
+    socket.leave(studioId);
+    console.log(`User ${socket.id} left studio: ${studioId}`);
+    // Remove the sessionId from the studio
+    if (studios.has(studioId)) {
+      const studio = studios.get(studioId);
+      const index = studio.indexOf(socket.id);
+      if (index > -1) {
+        studio.splice(index, 1);
+      }
+    }
+    // Send the occupancy of the studio to all connected users
+    const studio = studios.get(studioId);
+    io.to(studioId).emit('studio-occupancy', { occupancy: studio.length });
   });
 
   // Listen for the 'notify' event
@@ -49,6 +81,20 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log(`User ${socket.id} disconnected`);
+
+    // get the studio Id from the user to studio map
+    const studioId = userToStudio.get(socket.id);
+
+    // Remove the sessionId from the studio
+    if (studios.has(studioId)) {
+      const studio = studios.get(studioId);
+      const index = studio.indexOf(socket.id);
+      if (index > -1) {
+        studio.splice(index, 1);
+      }
+      // Send the occupancy of the studio to all connected users
+      io.to(studioId).emit('studio-occupancy', { occupancy: studio.length });
+    }
   });
 });
 
